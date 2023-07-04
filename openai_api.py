a05a04730e84ca6c902aeefaeaeb05e5d972c53e
OriAshkenazi <adiori13@gmail.com>
2023-07-04 17:34:56 +0300

    gpt-4 implementation

--- openai_api.py
+++ openai_api.py
@@ -3,8 +3,19 @@ import openai
 import langchain
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
-model = os.getenv('MODEL_SMART')
+gpt_model = os.getenv('MODEL_SMART')
 embedding_model = os.getenv('EMBEDDING_MODEL')
 
-def call_gpt4():
-    pass
+import openai
+
+def call_gpt4(prompt):
+    response = openai.ChatCompletion.create(
+        model=gpt_model,
+        messages=[
+            {"role": "system", "content": "You are an assistant helping to a complex code deciphering process."},
+            {"role": "user", "content": prompt},
+        ]
+    )
+    # The assistant's reply will be in the last message's 'content'
+    reply = response['choices'][0]['message']['content']
+    return reply
